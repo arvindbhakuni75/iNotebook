0a0a8f3c602fb99df371bc76d745bf23f459cc1a
Arvind Bhakuni <arvindbhakuni75@gmail.com>
2023-01-26 22:07:48 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
     <BrowserRouter>
     <Navbar />
       <Routes>
-        <Route path= "/" element = {<Home />} />
-        <Route path= "/about" element = {<About />} />
+        <Route exact path= "/" element = {<Home />} />
+        <Route exact path= "/about" element = {<About />} />
       </Routes>
     </BrowserRouter> 
       
