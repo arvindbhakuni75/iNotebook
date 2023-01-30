5570b16370731695e2e7cc9a4006f2d1b528b525
Arvind Bhakuni <arvindbhakuni75@gmail.com>
2023-01-30 13:15:05 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Signup from "./components/Signup";
 import { useState } from "react";
 
 function App() {
-
+  console.log("hello")
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
