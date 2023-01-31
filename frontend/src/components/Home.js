e17243d46f6865e10ac0abf6851f38e9406704e2
Arvind Bhakuni <arvindbhakuni75@gmail.com>
2023-01-31 22:54:11 +0530

    update

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Notes from "./Notes";
+
+
+const Home = (props) => {
+
+  const {showAlert} = props;
+
+  return (
+    <div className="row">
+      <div className="container">
+
+        <Notes showAlert = {showAlert} />
+        
+      </div>
+        
+      
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import {Link, useLocation, useNavigate} from 'react-router-dom';
+
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate.push('/login');
+  }
+  const location = useLocation();
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" href="#">iNotebook</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className={`nav-link ${location.pathname==="/"? "active":""}`} aria-current="page" to="/" >Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className={`nav-link ${location.pathname==="/about"? "active":""} `} to="/about">About</Link>
+              </li>
+            </ul>
+
+            {!localStorage.getItem('token')? <form className="d-flex">
+              <Link className="btn btn-success mx-1" to="/login" role="button">Login</Link>
+              <Link className="btn btn-success mx-1" to="/signup" role="button">Signup</Link>
+            </form> : <Link onClick={handleLogout} className="btn btn-success mx-1" to="/login" role="button">Logout</Link>}
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
